876d32c42c3e05f9f2678c5525b7e66284ff7473
angelalala <angela9004@gmail.com>
2024-05-14 11:17:23 -0700

    feat: flask api

---
+++ API/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import scraper
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/")
+def hello_world():
+    return "Hello, LinkedIn Scraper!"
+
+@app.route("/scrape", methods=['POST'])
+def scrape():
+    input = request.get_json()
+    if not input:
+        return jsonify({'error': 'No JSON data provided'}), 400
+    
+    url = input['url']
+    email = input['email']
+    password = input['password']
+
+    if not scraper.is_url_valid(url):
+        return jsonify({'error': 'Invalid LinkedIn URL'}), 400
+
+    opts = Options()
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=opts, service=service)
+
+    if not scraper.login(driver, email, password):
+        return jsonify({'error': 'Invalid LinkedIn credentials. Cannot continue scraping.'}), 400
+    
+    data, success = scraper.get_post_comments(driver, url)
+
+    if not success:
+        return jsonify({'error': 'LinkedIn post URL is not accessible.'}), 400
+    
+    driver.quit()
+
+    return data, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ API/scraper.py
@@ -0,0 +1,96 @@
+import requests
+import json
+import re
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def login(driver, email, password):
+    driver.get("https://www.linkedin.com")
+    user = driver.find_element(By.ID, "session_key")
+    user.send_keys(email)
+    sleep(0.5)
+    pw = driver.find_element(By.ID,'session_password')
+    pw.send_keys(password)
+    sleep(0.5)
+    sign_in_button = driver.find_element(By.XPATH,'//* [@type="submit"]')
+    sign_in_button.click()
+    
+    try:
+        error_message = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, "error-for-password"))
+        )
+        # print("Login Failed! Cannot continue scraping.")
+        driver.quit()
+        return False
+    except Exception:
+        print("Login Successful!")
+        sleep(2)
+    
+    return True
+
+def is_url_accessible(url):    
+    response = requests.get(url, timeout=5)
+    if response.status_code != 200:
+        return False
+    return True
+
+def is_url_valid(url):
+    if not re.match(r"https://www.linkedin.com/posts/.+", url):
+        return False
+    return True
+    
+def get_post_comments(driver, url):
+    if not is_url_accessible(url):
+        return {}, False
+
+    try:
+        driver.get(url)
+        wait = WebDriverWait(driver, 5)
+        while True:
+            load_more_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "comments-comments-list__load-more-comments-button")))
+            load_more_button.click()
+            sleep(3)
+    except Exception:
+        print("# All comments have been loaded")
+    finally:
+        data = []
+        comments = driver.find_elements(By.CLASS_NAME, "comments-comments-list__comment-item")
+        for comment in comments:
+            info = {}
+            commenter = comment.find_element(By.CLASS_NAME, "comments-post-meta__profile-info-wrapper")
+            commenterName = commenter.find_element(By.CLASS_NAME, "comments-post-meta__name-text").find_element(By.CSS_SELECTOR, "span[aria-hidden='true']").text
+            commenterLink = commenter.find_element(By.TAG_NAME, "a").get_attribute("href")
+            commenterHeadline = commenter.find_element(By.CLASS_NAME, "comments-post-meta__headline").text
+            commentContent = comment.find_element(By.CLASS_NAME, "comments-comment-item__main-content").text
+            info['Name'] = commenterName
+            info['Profile Link'] = commenterLink
+            info['Current Position'] = extract_position(commenterHeadline)
+            info['Comment'] = commentContent
+            data.append(info)
+        json_data = json.dumps(data)
+        return json_data, True
+
+def extract_position(commenterHeadline):
+    if commenterHeadline=="":
+        return "Not Available"
+    
+    if commenterHeadline.find("|") == -1:
+        return commenterHeadline
+    else:
+        return commenterHeadline[:commenterHeadline.find("|")].strip()
+
+# download or update ChromeDriver
+# opts = Options()
+# opts.add_argument("--headless")
+# opts.add_argument("--disable-gpu")
+# service = Service(ChromeDriverManager().install())
+# driver = webdriver.Chrome(options=opts, service=service)
+
+# is_url_valid(url)
+# login(driver, email, password)
+# get_post_comments(driver, url)
+
+# close the browser
+# driver.quit()
